b1c5655f6ac5429ccd84871f9185fd50cd2c49b9
OgnjenPapovic <149535461+OgnjenPapovic@users.noreply.github.com>
2024-03-12 16:34:00 +0100

    Errors fixed

--- View/CreatingTour.xaml.cs
+++ View/CreatingTour.xaml.cs
@@ -39,15 +39,19 @@ namespace BookingApp.View
             }
         }
 
-        private void SetKeyPoints(int tourId)
+        private bool SetKeyPoints(int tourId)
         {
-          
-            
             string keyPointsText = KeyPointsTextBox.Text.Trim();
             if (!string.IsNullOrEmpty(keyPointsText))
             {
                 string[] keyPointsArray = keyPointsText.Split(',');
                 int totalKeyPoints = keyPointsArray.Length;
+
+                if (totalKeyPoints < 2 || keyPointsArray[1]=="")
+                {
+                    MessageBox.Show("Please enter at least two key points separated by commas.");
+                    return false;
+                }
 
                 for (int i = 0; i < totalKeyPoints; i++)
                 {
@@ -58,27 +62,53 @@ namespace BookingApp.View
 
                     keyPointsRepository.AddKeyPoint(tourId, keyPointName, keyType, ordinalNumber, isChecked);
                 }
+
+                return true;
             }
-
-          
+            else
+            {
+                MessageBox.Show("Please enter at least two key points separated by commas.");
+                return false;
+            }
         }
-
-        
 
         private void CreateTourButton_Click(object sender, RoutedEventArgs e)
         {
-           
-                TourDto newTourDto = new TourDto(_tourDto.Name, _tourDto.Description, _tourDto.Language, _tourDto.MaxTouristNumber, _tourDto.StartDate, _tourDto.Duration, _tourDto.LocationDto,_tourDto.ImagesPaths);
-                tourRepository.AddTour(newTourDto.ToTour());
-                int id = tourRepository.NextId()-1;
-                SetKeyPoints(id);
+            if (!ValidateFields())
+            {
+                return;
+            }
+
+            TourDto newTourDto = new TourDto(_tourDto.Name, _tourDto.Description, _tourDto.Language, _tourDto.MaxTouristNumber, _tourDto.StartDate, _tourDto.Duration, _tourDto.LocationDto, _tourDto.ImagesPaths);
+            tourRepository.AddTour(newTourDto.ToTour());
+            int id = tourRepository.NextId() - 1;
+
+            if (SetKeyPoints(id))
+            {
                 MessageBox.Show("Tour created successfully!");
                 this.Close();
-            
-            
+            }
         }
 
-      
+
+
+        private bool ValidateFields()
+        {
+            if (string.IsNullOrEmpty(_tourDto.Name) ||
+                string.IsNullOrEmpty(_tourDto.Description) ||
+                string.IsNullOrEmpty(_tourDto.Language) ||
+                _tourDto.MaxTouristNumber <= 0 ||
+                _tourDto.Duration <= 0 ||
+                string.IsNullOrEmpty(_tourDto.LocationDto.Country) ||
+                string.IsNullOrEmpty(_tourDto.LocationDto.City) ||
+                _tourDto.ImagesPaths.Count == 0)
+            {
+                MessageBox.Show("Please fill in all required fields.");
+                return false;
+            }
+            return true;
+        }
+
 
     }
 }
