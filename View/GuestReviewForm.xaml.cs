257ac162cc3b5b539650af897269db367f614288
Marija Djurdjevic <masadjurdjevic02@gmail.com>
2024-03-15 21:00:01 +0100

    Save review

--- View/GuestReviewForm.xaml.cs
+++ View/GuestReviewForm.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using BookingApp.Dto;
+using BookingApp.Repository;
+
+
 namespace BookingApp.View
 {
     /// <summary>
@@ -20,22 +23,35 @@ namespace BookingApp.View
     public partial class GuestReviewForm : Window
     {
         private PropertyReservationDto _propertyReservationDto;
+        private ReviewDto _reviewDto;
+
+        ReviewRepository reviewRepository;
         public GuestReviewForm()
         {
             InitializeComponent();
+            _reviewDto = new ReviewDto();
+            DataContext = _reviewDto;
+            reviewRepository = new ReviewRepository();
         }
         public GuestReviewForm(PropertyReservationDto propertyReservationDto)
         {
             InitializeComponent();
+            _reviewDto = new ReviewDto();
+            DataContext = _reviewDto;
+            reviewRepository = new ReviewRepository();
             _propertyReservationDto = propertyReservationDto;
 
             FirstNameTextBox.Text = _propertyReservationDto.GuestFirstName;
             LastNameTextBox.Text = _propertyReservationDto.GuestLastName;
+
         }
 
         public void SaveReview_Click(Object sender, EventArgs e)
         {
-
+            ReviewDto newReviewDto = new ReviewDto(_reviewDto.ReservationId, _reviewDto.GuestId,_reviewDto.Cleanliness, _reviewDto.Rules, _reviewDto.Comment);
+            reviewRepository.AddReview(newReviewDto.ToReview());
+            MessageBox.Show("Review created successfully!");
+            this.Close();
         }
     }
 }
