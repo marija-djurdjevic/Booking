7405b304cafc06bc2a01ebd12d54c7ab0a8803c8
janaajankovic <janajankoviic01@gmail.com>
2024-04-10 19:25:19 +0200

    make request to change reservation

--- GuestView/ChangeReservation.xaml.cs
+++ GuestView/ChangeReservation.xaml.cs
@@ -1,18 +1,10 @@
-﻿using BookingApp.Model;
+﻿using BookingApp.Dto;
+using BookingApp.Model;
+using BookingApp.Repository;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections.ObjectModel;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace BookingApp.GuestView
 {
@@ -21,10 +13,14 @@ namespace BookingApp.GuestView
     /// </summary>
     public partial class ChangeReservation : Page
     {
-
+        public ReservationChangeRequestsRepository ReservationChangeRequestsRepository { get; set; }
         public PropertyReservation SelectedReservation { get; set; }
+        public ObservableCollection<ReservationChangeRequest> GuestsRequests {  get; set; }
         public Property SelectedProperty { get; set; }
         public Guest LoggedInGuest { get; set; }
+        public DateTime FromDate {  get; set; }
+        public DateTime ToDate { get; set; }
+        public ReservationChangeRequestDto ReservationChangeRequest { get; set; }
         public ChangeReservation(PropertyReservation selectedReservation, Property selectedProperty, Guest guest)
         {
             InitializeComponent();
@@ -32,6 +28,40 @@ namespace BookingApp.GuestView
             SelectedReservation = selectedReservation;
             LoggedInGuest = guest;
             SelectedProperty = selectedProperty;
+            ReservationChangeRequest = new ReservationChangeRequestDto();
+            ReservationChangeRequestsRepository = new ReservationChangeRequestsRepository();
+            GuestsRequests = new ObservableCollection<ReservationChangeRequest>(ReservationChangeRequestsRepository.GetAll().FindAll(r => r.GuestId == LoggedInGuest.Id));
+
+        }
+
+        private void DatePicker_SelectedDate1Changed(object sender, SelectionChangedEventArgs e)
+        {
+            if (sender is DatePicker datePicker)
+            {
+                FromDate = datePicker.SelectedDate ?? DateTime.Now;
+            }
+        }
+
+        private void DatePicker_SelectedDate2Changed(object sender, SelectionChangedEventArgs e)
+        {
+            if (sender is DatePicker datePicker)
+            {
+                ToDate = datePicker.SelectedDate ?? DateTime.Now;
+            }
+        }
+
+        private void SendRequest_Click(object sender, RoutedEventArgs e)
+        {
+            ReservationChangeRequest.OldStartDate = SelectedReservation.StartDate;
+            ReservationChangeRequest.OldEndDate = SelectedReservation.EndDate;
+            ReservationChangeRequest.NewStartDate = FromDate;
+            ReservationChangeRequest.NewEndDate = ToDate;
+            ReservationChangeRequest.PropertyName = SelectedProperty.Name;
+            ReservationChangeRequest.GuestId = LoggedInGuest.Id;
+            ReservationChangeRequestsRepository.AddReservationChangeRequest(ReservationChangeRequest.ToReservationChangeRequest());
+            GuestsRequests.Clear();
+            ReservationChangeRequestsRepository.GetAll().FindAll(r => r.GuestId == LoggedInGuest.Id).ForEach(GuestsRequests.Add);
+            MessageBox.Show("Request sent!");
 
         }
     }
