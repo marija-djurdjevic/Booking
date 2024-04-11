9752f71cc69d1a1bea725aec7567ac8fa00ce731
janaajankovic <janajankoviic01@gmail.com>
2024-04-11 21:12:57 +0200

    cancel reservation

---
+++ GuestView/OwnerReview.xaml.cs
@@ -0,0 +1,22 @@
+﻿using BookingApp.Model;
+using System.Windows.Controls;
+
+namespace BookingApp.GuestView
+{
+    /// <summary>
+    /// Interaction logic for OwnerReview.xaml
+    /// </summary>
+    public partial class OwnerReview : Page
+    {
+        public PropertyReservation SelectedReservation { get; set; }
+        public Property SelectedProperty { get; set; }
+        public Guest LoggedInGuest { get; set; }
+        public OwnerReview(PropertyReservation selectedReservation, Property selectedProperty, Guest guest)
+        {
+            InitializeComponent();
+            SelectedReservation = selectedReservation;
+            SelectedProperty = selectedProperty;
+            LoggedInGuest = guest;
+        }
+    }
+}
