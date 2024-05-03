25a4d985cbbbe381bb03b7e1fb632e2ed12f0979
OgnjenPapovic <149535461+OgnjenPapovic@users.noreply.github.com>
2024-05-03 19:10:01 +0200

    styling

---
+++ WPF/Converters/DateRangeConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace BookingApp.WPF.Converters
+{
+    public class DateRangeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is (DateTime startDate, DateTime endDate))
+            {
+                return $"{startDate:dd.MM.yyyy HH:mm}h - {endDate:dd.MM.yyyy HH:mm}h";
+            }
+            return string.Empty;
+        }
+
+
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
