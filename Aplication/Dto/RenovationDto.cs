593270cc6719a034ac77899515a3ce116e90c4fc
Marija Djurdjevic <masadjurdjevic02@gmail.com>
2024-05-08 01:28:11 +0200

    Add renovation page

---
+++ Aplication/Dto/RenovationDto.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using BookingApp.Domain.Models;
+using System.ComponentModel;
+using System.Data;
+
+
+namespace BookingApp.Aplication.Dto
+{
+    public class RenovationDto : INotifyPropertyChanged
+    {
+        private int id;
+        private int ownerId;
+        private int propertyId;
+        private DateTime startDate;
+        private DateTime endDate;
+        private string description;
+        private int duration;
+
+        public RenovationDto() { }
+        public RenovationDto(int id, int ownerId, int propertyId, DateTime startDate, DateTime endDate, string description, int duration)
+        {
+            this.id = id;
+            this.ownerId = ownerId;
+            this.propertyId = propertyId;
+            this.startDate = startDate;
+            this.endDate = endDate;
+            this.description = description;
+            this.duration = duration;
+        }
+        public RenovationDto(int ownerId, int propertyId, DateTime startDate, DateTime endDate, string description, int duration)
+        {
+            this.ownerId = ownerId;
+            this.propertyId = propertyId;
+            this.startDate = startDate;
+            this.endDate = endDate;
+            this.description = description;
+            this.duration = duration;
+        }
+        public RenovationDto(Renovation renovation)
+        {
+            ownerId = renovation.OwnerId;
+            propertyId = renovation.PropertyId;
+            startDate = renovation.StartDate;
+            endDate = renovation.EndDate;
+            description = renovation.Description;
+            duration = renovation.Duration;
+
+        }
+        public int OwnerId
+        {
+
+            get { return ownerId; }
+            set
+            {
+                if (value != ownerId)
+                {
+                    ownerId = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public int PropertyId
+        {
+
+            get { return propertyId; }
+            set
+            {
+                if (value != propertyId)
+                {
+                    propertyId = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public DateTime StartDate
+        {
+
+            get { return startDate; }
+            set
+            {
+                if (value != startDate)
+                {
+                    startDate = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public DateTime EndDate
+        {
+
+            get { return endDate; }
+            set
+            {
+                if (value != endDate)
+                {
+                    endDate = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public string Description
+        {
+
+            get { return description; }
+            set
+            {
+                if (value != description)
+                {
+                    description = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public int Duration
+        {
+
+            get { return duration; }
+            set
+            {
+                if (value != duration)
+                {
+                    duration = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public Renovation ToRenovation()
+        {
+            return new Renovation(OwnerId, PropertyId, StartDate, EndDate, Description, Duration);
+        }
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ WPF/Views/OwnerView/ScheduleRenovationPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using BookingApp.Domain.Models;
+using BookingApp.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BookingApp.WPF.Views.OwnerView
+{
+    /// <summary>
+    /// Interaction logic for ScheduleRenovationPage.xaml
+    /// </summary>
+    public partial class ScheduleRenovationPage : Page
+    {
+        User LoggedInUser;
+        private readonly PropertyRepository _propertyRepository;
+        public List<string> AllPropertyNames { get; set; }
+
+        public ScheduleRenovationPage(User loggedInUser)
+        {
+            InitializeComponent();
+            LoggedInUser = loggedInUser;
+            _propertyRepository = new PropertyRepository();
+            Loaded += ScheduleRenovationPage_Loaded;
+        }
+        private void ScheduleRenovationPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            AllPropertyNames = _propertyRepository.GetAllPropertyNames();
+            DataContext = this;
+        }
+    }
+}
