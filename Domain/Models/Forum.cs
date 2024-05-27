574b6e3e8ad773d7705702aead3b9a8575c4e7a9
janaajankovic <janajankoviic01@gmail.com>
2024-05-27 17:26:42 +0200

    open forum

---
+++ Domain/Models/Forum.cs
@@ -0,0 +1,43 @@
+﻿using BookingApp.Serializer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingApp.Domain.Models
+{
+    public class Forum : ISerializable
+    {
+        public int Id { get; set; }
+        public int GuestId { get; set; }
+        Location Location { get; set; }
+        public string Comment;
+
+        public Forum()
+        {
+        }
+
+        public Forum(int guestId, Location location, string comment)
+        {
+            GuestId = guestId;
+            Location = location;
+            Comment = comment;
+        }
+
+        public string[] ToCSV()
+        {
+            string[] csvValues = { Id.ToString(), GuestId.ToString(), Location.Country, Location.City, Comment };
+            return csvValues;
+        }
+
+        public void FromCSV(string[] values)
+        {
+            Id = Convert.ToInt32(values[0]);
+            GuestId = Convert.ToInt32(values[1]);
+            Location.Country = values[2];
+            Location.City = values[3];
+            Comment = values[4];
+        }
+    }
+}
